ade46c57f4c76cf15305840aa5c006767b3f2ba8
Andrew <166739925+lawsy-2001@users.noreply.github.com>
2024-05-13 10:43:05 +0100

    added playRound function
    This takes the human and computer selection and
    compares the choices and decides who has won. The
    winners score is increased by one if they win.

--- script.js
+++ script.js
@@ -17,3 +17,32 @@ function getHumanChoice(){
 
 let humanScore = 0;
 let computerScore = 0;
+
+function playRound(humanChoice, computerChoice){
+    if (humanChoice === computerChoice){
+        console.log(`It's a draw! you both chose ${humanChoice}`);
+    } else if (humanChoice === 'rock' && computerChoice === 'paper'){
+        console.log(`You lose! ${computerChoice} beats ${humanChoice}`);
+        computerScore += 1;
+    } else if (humanChoice === 'rock' && computerChoice === 'scissors'){
+        console.log(`You win! ${humanChoice} beats ${computerChoice}`);
+        humanScore += 1;
+    } else if (humanChoice === 'paper' && computerChoice === 'scissors'){
+        console.log(`You lose! ${computerChoice} beats ${humanChoice}`);
+        computerScore += 1;
+    } else if (humanChoice === 'paper' && computerChoice === 'rock'){
+        console.log(`You win! ${humanChoice} beats ${computerChoice}`);
+        humanScore += 1;
+    } else if (humanChoice === 'scissors' && computerChoice === 'rock'){
+        console.log(`You lose! ${computerChoice} beats ${humanChoice}`);
+        computerScore += 1;
+    } else if (humanChoice === 'scissors' && computerChoice === 'paper'){
+        console.log(`You win! ${humanChoice} beats ${computerChoice}`);
+        humanScore += 1;
+    }
+}
+
+const humanSelection = getHumanChoice().toLowerCase();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
